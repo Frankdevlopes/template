cd1e6b9b6c16f0861261a1b02e555700fea09be4
francisMwaura <frankdevlopes@gmail.com>
2024-02-04 19:46:27 +0300

    second commit

--- medicorp.js
+++ medicorp.js
@@ -9,10 +9,4 @@ document.addEventListener('DOMContentLoaded', function() {
       }, 2000); // Adjust time as needed
     }, 3000); // Loader display time
   });
-  // Example JavaScript for handling sign-in on the sign-in page
-
-  document.getElementById('sign-in-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
-    // Perform sign-in logic here...
-    window.location.href = 'dashboard.html'; // Redirect to the dashboard upon successful sign-in
-});
+  
